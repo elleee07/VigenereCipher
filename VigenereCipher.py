b4c11993935b62f6a825b6339a63634a14a1e33f
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-04-21 14:38:29 +0800

    Define Ciphertext

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -11,8 +11,16 @@ def generateKeyword(textMessage, key):
     else:
         for i in range(len(textMessage) - len(key)):
             key.append(key[i % len(key)])
-        return("".join(key))
-    
-# define the cipher text to generate it and will add modulo 26 to complete the alphabet
+        return("".join(key))  
+# define the cipher text
+def cipherText(textMessage, key):
+    cipher_text = []
+    for i in range(len(textMessage)):
+        x = (ord(textMessage[i]) + ord(key[i])) % 26
+        x += ord('A')
+        cipher_text.append(chr(x)) 
+    return("" .join(cipher_text))
+
+
 # ask the user to type any message, keyword in all capital letters with no spaces
 # added some colors
